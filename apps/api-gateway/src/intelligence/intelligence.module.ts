a329e5b0455b318bb19df1778497066e07b9d84a
Admin <admin@Admins-MacBook-Pro.local>
2026-06-29 23:50:46 -0300

    fix: Importa AuthModule no IntelligenceModule

--- apps/api-gateway/src/intelligence/intelligence.module.ts
+++ apps/api-gateway/src/intelligence/intelligence.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { IntelligenceController } from './intelligence.controller';
 import { IntelligenceService } from './intelligence.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [IntelligenceController],
   providers: [IntelligenceService],
   exports: [IntelligenceService],
 })
 export class IntelligenceModule {}
+
